72b415e2570df9d1dde97a0e77f0fb15eb6ba13c
trinhhieu <trantrinhhieu2908@gmail.com>
2023-10-06 00:09:54 +0700

    feat: Add Layout component

--- src/shared/components/Layout/Layout.tsx
+++ src/shared/components/Layout/Layout.tsx
@@ -1,5 +1,22 @@
+import { Outlet } from 'react-router-dom';
+import Header from '../Header';
+import Footer from '../Footer';
+import Grid from '@mui/material/Grid';
+
 const Layout = () => {
-  return <div>Layout</div>;
+  return (
+    <>
+      <Header />
+      <Grid container className='tw-h-full tw-bg-background tw-mt-20'>
+        <Grid item xs={6} md={1}></Grid>
+        <Grid item xs={6} md={10}>
+          <Outlet />
+        </Grid>
+        <Grid item xs={6} md={1}></Grid>
+      </Grid>
+      <Footer />
+    </>
+  );
 };
 
 export default Layout;
