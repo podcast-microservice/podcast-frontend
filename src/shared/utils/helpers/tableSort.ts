8ae0256f230ba1f22005cbcea7e5ef256645a0de
trinhhieu <trantrinhhieu2908@gmail.com>
2023-10-06 00:15:36 +0700

    feat: Add components for SortTable

---
+++ src/shared/utils/helpers/tableSort.ts
@@ -0,0 +1,33 @@
+import { Order } from '../models/tableSort';
+
+function descendingComparator<T, K extends keyof T>(a: T, b: T, orderBy: K) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+export function getComparator<T, K extends keyof T>(order: Order, orderBy: K): (a: T, b: T) => number {
+  return order === 'desc'
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+// Since 2020 all major browsers ensure sort stability with Array.prototype.sort().
+// stableSort() brings sort stability to non-modern browsers (notably IE11). If you
+// only support modern browsers you can replace stableSort(exampleArray, exampleComparator)
+// with exampleArray.slice().sort(exampleComparator)
+export function stableSort<T>(array: readonly T[], comparator: (a: T, b: T) => number) {
+  const stabilizedThis = array.map((el, index) => [el, index] as [T, number]);
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0]);
+    if (order !== 0) {
+      return order;
+    }
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map((el) => el[0]);
+}
