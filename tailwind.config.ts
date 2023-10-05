ea137a11578a18d4e941de451798035130dae785
trinhhieu <trantrinhhieu2908@gmail.com>
2023-10-05 23:58:17 +0700

    chore: Update Tailwind CSS configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,7 @@ const config = {
       colors: {
         border: 'hsl(var(--border))',
         input: 'hsl(var(--input))',
+        footer: 'hsl(var(--footer))',
         ring: 'hsl(var(--ring))',
         background: 'hsl(var(--background))',
         foreground: 'hsl(var(--foreground))',
