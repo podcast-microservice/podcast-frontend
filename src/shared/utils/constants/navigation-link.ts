e062f3401c221fa0d58a67bdc4d9a53821f03f5f
trinhhieu <trantrinhhieu2908@gmail.com>
2023-10-06 00:17:02 +0700

    feat: Update AppRoute and Add mainNavigationLinks

---
+++ src/shared/utils/constants/navigation-link.ts
@@ -0,0 +1,33 @@
+import { routes } from '~/routes';
+
+export interface NavigationLink {
+  title: string;
+  path: string;
+}
+
+export const mainNavigationLinks: Array<NavigationLink> = [
+  {
+    title: 'Home',
+    path: routes.homePath
+  },
+  {
+    title: 'Podcast',
+    path: routes.podcastPath
+  },
+  {
+    title: 'Course',
+    path: routes.coursePath
+  },
+  {
+    title: 'Blog',
+    path: routes.blogPath
+  },
+  {
+    title: 'News',
+    path: routes.newsPath
+  },
+  {
+    title: 'Components',
+    path: routes.componentPath
+  }
+];
